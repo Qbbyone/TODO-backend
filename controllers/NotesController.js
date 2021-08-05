f7616c8fffba5acffe8b9790250cc454a742d471
Olga Matveeva <qbbyone8@mail.ru>
2021-08-05 18:41:39 +0300

    UPDATED: sorting

--- controllers/NotesController.js
+++ controllers/NotesController.js
@@ -1,22 +1,11 @@
-const noteService = require("../services/NotesService");
-const tagService = require("../services/TagsService")
 const userService = require("../services/UserService");
-const tagDto = require("../dto/tag.dto");
-const noteDto = require("../dto/note.dto");
+const getData = require("../data");
 
 class NotesController {
   async loadNoteList(req, res) {
     try {
       const userId = await userService.getUserIdByToken(req.query.token);
-      
-      // get tagList
-      const tagList = await tagService
-        .getTagsByUserId(userId)
-        .then((tagList) => tagList.map((tag) => tagDto(tag)));
-      
-      const noteList = await noteService
-        .getNotesByUserId(userId)
-        .then((noteList) => noteList.map((note) => noteDto(note, JSON.parse(JSON.stringify(tagList)))));
+      const [tagList, noteList] = await getData(userId);
       res.json(noteList);
     } catch (e) {
       console.log("NOTELIST ERROR", e);
---
+++ data.js
@@ -0,0 +1,33 @@
+const tagService = require("./services/TagsService");
+const noteService = require("./services/NotesService");
+const tagDto = require("./dto/tag.dto");
+const noteDto = require("./dto/note.dto");
+
+async function getData(userId) {
+  const activeTags = [];
+
+  const tags = await tagService.getTagsByUserId(userId).then((tags) =>
+    tags.map((tag) => {
+      console.log(tag);
+      if (tag.is_active) activeTags.push(tag.tag_id);
+      return tagDto(tag);
+    })
+  );
+  console.log(activeTags);
+  console.log();
+  const notes = await noteService
+    .getNotesByUserId(userId)
+    .then((notes) =>
+      notes
+        .filter(
+          (note) =>
+            !activeTags.length ||
+            note.note_tags.some((el) => activeTags.includes(el))
+        )
+        .map((note) => noteDto(note, JSON.parse(JSON.stringify(tags))))
+    );
+
+  return [tags, notes];
+}
+
+module.exports = getData;
